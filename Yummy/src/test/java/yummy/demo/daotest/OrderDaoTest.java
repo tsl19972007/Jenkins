7fe55e83df3f9e6f2d2c930d04fe34d64610107f
tsl19972007 <498656288@qq.com>
2019-05-19 16:24:21 +0800

    test

--- Yummy/src/test/java/yummy/demo/daotest/OrderDaoTest.java
+++ Yummy/src/test/java/yummy/demo/daotest/OrderDaoTest.java
@@ -5,9 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderDaoTest {
-
+    @Test
+    public void create() throws Exception {
+        assertTrue(true);
+    }
 }
 
